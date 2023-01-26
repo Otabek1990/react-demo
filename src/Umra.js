d46020ea0674a087c7b96ff796f19fcc5caee460
otabek <fozilovotabek424@gmail.com>
2023-01-26 14:58:04 +0500

    tailwind added in project

---
+++ src/Umra.js
@@ -0,0 +1,27 @@
+  // const baseURL = "https://sunnatour.herokuapp.com/api/v1/auth/"
+
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   const postData = {
+  //     // ism: "ozod",
+  //     // mobile: 5875355,
+  //     // password: "1111"
+  //     "mobile": "998908151808",
+  //     "lang": "uz"
+  //   };
+  //   axios.post(baseURL, {
+  //     method: "step.one",
+  //     params: postData,
+  //     headers: {
+  //       'Access-Control-Allow-Origin': '*',
+  //       'Content-Type': 'application/json',
+  //     },
+  //   })
+  //   .then(res=>{
+  //     console.log(res.data)
+  //     localStorage.setItem("token",res.data?.token)
+  //   }
+  //     )
+  //   .catch(err=>console.log(err.message))
+
+  // }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Navbar(props) {
+  return (
+    <nav className="bg-red-400 py-4 px-20 sticky top-0 z-50">
+   
+     <button className="bg-slate-800 bg- text-white p-3">enter</button>
+    </nav>
+  );
+}
+
+export default Navbar;
